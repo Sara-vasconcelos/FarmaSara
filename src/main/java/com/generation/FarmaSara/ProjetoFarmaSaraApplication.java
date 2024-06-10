1a0e01d46ebb6e95de1edd6113672d7ef5c4eb0c
Sara <vasconcelossara11@gmail.com>
2024-06-10 10:51:16 -0300

    Primeira etapa - configurando o prjeto

---
+++ src/main/java/com/generation/FarmaSara/ProjetoFarmaSaraApplication.java
@@ -0,0 +1,13 @@
+package com.generation.FarmaSara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoFarmaSaraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoFarmaSaraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/generation/FarmaSara/ProjetoFarmaSaraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.FarmaSara;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoFarmaSaraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
